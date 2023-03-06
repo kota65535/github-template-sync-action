13e47740e1fdec845d594e826a3ba090b6b1d8b6
Tomohiko Ozawa <kota65535@gmail.com>
2023-03-07 04:04:29 +0900

    created

--- dist/index.js
+++ dist/index.js
@@ -16255,6 +16255,7 @@ const exec = (file, options) => {
   core.info(`running command: ${file} ${(options || []).join(" ")}`);
   const res = execa.sync(file, options);
   core.debug(res.stdout);
+  core.debug(res.stderr);
   return res;
 };
 
--- src/exec.js
+++ src/exec.js
@@ -5,6 +5,7 @@ const exec = (file, options) => {
   core.info(`running command: ${file} ${(options || []).join(" ")}`);
   const res = execa.sync(file, options);
   core.debug(res.stdout);
+  core.debug(res.stderr);
   return res;
 };
 
